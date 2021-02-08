52d191e6fcc331d194ff382fc2c9ebbbdc153e61
supanat <supanat.ap@gmail.com>
2021-02-08 17:41:10 +0700

    a

--- 105_lab2.2.c
+++ 105_lab2.2.c
@@ -2,24 +2,32 @@
 int main()
 {
     int a,c;
-    float b,sum;
+    float b,sum,pay1=199,pay2=299;
     char ch;
 
     scanf("%c",&ch);
     scanf("%d.%f",&a,&b);
     b=b/100;
     sum=a+b;
+
     printf("%.2f",sum);
 
 
-    /*if(ch=='A')
+    if(ch=='A')
     {
+        if(sum>200)
+        {
 
+        }
+        else
+        {
+            printf("%.2f",pay1);
+        }
 
     }
     else if(ch=='B')
     {
 
     }
-    */
+
 }
