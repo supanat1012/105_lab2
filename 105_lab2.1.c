fda33b927c08235ff993bf1a91c93e80b80f28f7
supanat <supanat.ap@gmail.com>
2021-02-08 16:47:36 +0700

    Update 105_lab2.1.c

--- 105_lab2.1.c
+++ 105_lab2.1.c
@@ -6,3 +6,22 @@ int main()
     scanf("%d",&a);
     scanf("%d",&b);
     scanf("%d",&c);
+
+    if(0<=a<=30)
+    {
+        if(0<=b<=30)
+        {
+            if(0<=c<=40)
+            {
+                sum=a+b+c;
+
+                if()
+
+
+
+
+
+
+            }
+        }
+    }
